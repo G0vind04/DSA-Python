7a7f7c61444e3b651e5eb01737ffee99ddd84d87
govind <govindmohan2004@gmail.com>
2025-07-17 00:27:34 +0530

    tried out the get function to obtain frequency

--- freq_map.py
+++ freq_map.py
@@ -8,3 +8,12 @@ for i in range(len(num)):
     else:
         freq_map[num[i]]=1
 print(freq_map)
+
+#using hashmaps get
+
+num1=[5,6,7,7,1,9,11,1,1,5,1,1]
+hash_map={}
+n=len(num)
+for i in range(n):
+    hash_map[num[i]]=hash_map.get(num[i],0)+1
+print(hash_map)
